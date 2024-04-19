e1fc5670cfa46a257ed7224b2f8efb08e16ca0b1
Christian <Christian>
2024-04-19 17:54:16 -0400

    updated simple shell

---
+++ simple_shell.c
@@ -0,0 +1,36 @@
+#include "main.h" 
+
+/**
+ * 
+ * 
+ * 
+ * 
+*/
+
+int main()
+{
+	ssize_t read;
+	char *buffer = NULL, *token;
+	char *delim = " ";
+	size_t size = 0;
+
+	while(1)
+	{
+		printf("$ ");
+		read = getline(&buffer, &size, stdin);
+		if (read == -1)
+		{
+			printf("C ya later, don't forget your coffee!\n");
+			free(buffer);
+			return(0);
+		}
+		token = strtok(buffer, delim);
+		while (token != NULL)
+		{
+			printf("%s\n", token);
+			token = strtok(NULL, delim);
+		}
+
+	}
+	return (0);
+}
