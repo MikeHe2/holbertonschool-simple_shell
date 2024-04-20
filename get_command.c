8fb133ef54dcd9c275fc132fdf97fa8e3e51d7af
Christian <Christian>
2024-04-19 20:32:37 -0400

    added execution in get command and integrated it to simple shell

--- get_command.c
+++ get_command.c
@@ -8,9 +8,27 @@
  * 
 */
 
-int get_command()
+int get_command(char *token)
 {
-	char *argv;
+    pid_t son;
 
-	argv = execve();
+	son = fork();
+	if (son < 0)
+	{
+		perror("fork failure");
+		return(-1);
+	}
+	else if (son == 0)
+	{
+		if (execve(token[0], token, NULL) < 0)
+		{
+			printf("command not found\n");
+		}
+		exit(0);
+	}
+	else
+	{
+		wait(NULL);
+	}
+	return (0);
 }
