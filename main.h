e07825e8874e63a3bdb3eefc0566e4c59491f360
Michael A. Hernandez <michael.he30@hotmail.com>
2024-04-20 23:04:14 -0400

    Alex code modified (TEST)

--- main.h
+++ main.h
@@ -1,9 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include<stdio.h> 
-#include<string.h> 
-#include<stdlib.h> 
-#include<unistd.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <stdbool.h>
+
+char* _strtok(char *str);
 
 #endif
+
