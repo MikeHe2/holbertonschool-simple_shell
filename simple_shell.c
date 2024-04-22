baabed9221dfc3b1e1b167ba196224bb4e5e3743
Christian <Christian>
2024-04-22 10:45:15 -0400

    Added exit function

--- simple_shell.c
+++ simple_shell.c
@@ -1,56 +1,72 @@
-#include "main.h" 
+#include "main.h"
 
 /**
  * main - makes a prompt and waits for command
- * 
+ *
  * Return: exits the prompt
- * 
+ *
 */
 
-int main(void) {
-  char *buffer = NULL;
-  char *args[2];
-  ssize_t bytes_read, read;
-  pid_t child;
-  size_t size = 0;
+int main(void)
+{
+	char *buffer = NULL;
+	char *args[2];
+	ssize_t bytes_read, read;
+	pid_t child;
+	size_t size = 0;
 
-  printf("Welcome to simple_shell use it if you dare :)\n");
+	printf("Welcome to simple_shell use it if you dare :)\n");
 
-  while (1) {
-    printf("$ ");
-    read = getline(&buffer, &size, stdin);
-    if (read == -1)
+	while (1)
 	{
-      perror("C ya later! Don't forget your coffee!");//ctrl + d
-      exit(EXIT_FAILURE);
-    }
+		printf("$ ");
+		read = getline(&buffer, &size, stdin);
+		if (read == -1)
+		{
+			perror("C ya later! Don't forget your coffee!");//ctrl + d
+			exit(EXIT_FAILURE);
+		}
 
-    buffer[read - 1] = '\0';
+		buffer[read - 1] = '\0';
 
-    args[0] = buffer;
-    args[1] = NULL;
+		args[0] = buffer;
+		args[1] = NULL;
+		if (strcmp(args[0], "exit") == 0)
+		{
+			break;
+		}
 
-    child = fork();
-    if (child == -1) {
-      perror("fork not succesful");
-      exit(EXIT_FAILURE);
-    } else if (child == 0) {
-      char *path = get_command(args[0]);
-      if (path != NULL) {
-        execve(path, args, NULL);
-      } else {
-        printf("%s: command not found\n", buffer);
-      }
-      free(path);
-      exit(EXIT_FAILURE);
-    } else {
-      int status;
-      if (waitpid(child, &status, 0) == -1) {
-        perror("waitpid");
-        exit(EXIT_FAILURE);
-      }
-    }
-  }
+		child = fork();
+		if (child == -1)
+		{
+			perror("fork not succesful");
+			exit(EXIT_FAILURE);
+		}
+		else if (child == 0)
+		{
+			char *path = get_command(args[0]);
 
-  return EXIT_SUCCESS;
+			if (path != NULL) 
+			{
+				execve(path, args, environ);
+				perror(buffer);
+			}
+			else
+			{
+				printf("%s: command not found\n", buffer);
+			}
+			free(path);
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			int status;
+			if (waitpid(child, &status, 0) == -1)
+			{
+				perror("waitpid");
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+	return EXIT_SUCCESS;
 }
