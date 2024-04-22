0d47bba551992eb3b9c8ed36444f80d996d80937
Christian <Christian>
2024-04-21 20:17:42 -0400

    added command functionality without writing the path

--- get_command.c
+++ get_command.c
@@ -8,27 +8,21 @@
  * 
 */
 
-int get_command(char *token)
-{
-    pid_t son;
+char* get_command(char *cmd_name) {
+  // Check for the command in common locations (modify as needed)
+  static char *locations[] = {"/bin", "/usr/bin", NULL};
 
-	son = fork();
-	if (son < 0)
-	{
-		perror("fork failure");
-		return(-1);
-	}
-	else if (son == 0)
-	{
-		if (execve(token[0], token, NULL) < 0)
-		{
-			printf("command not found\n");
-		}
-		exit(0);
-	}
-	else
-	{
-		wait(NULL);
-	}
-	return (0);
+  for (int i = 0; locations[i] != NULL; i++) {
+    char *full_path = malloc(strlen(locations[i]) + strlen(cmd_name) + 2);
+    sprintf(full_path, "%s/%s", locations[i], cmd_name);
+    if (access(full_path, X_OK) == 0) {
+      return full_path;
+    }
+    free(full_path);
+  }
+
+  // Check environment variable PATH (more complex parsing needed)
+  // ...
+
+  return NULL; // Executable not found
 }
