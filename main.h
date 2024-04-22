57a615c1fccc7c551da918748cc5c60d6e648af5
Michael A. Hernandez <michael.he30@hotmail.com>
2024-04-22 19:52:25 -0400

    Maneja Exit, imprime comandos mas de una vez

--- main.h
+++ main.h
@@ -1,15 +1,9 @@
-#ifndef MAIN_H
+#define MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <stdbool.h>
-
-char* get_command(char *cmd_name);
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
 
 #endif
