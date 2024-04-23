837bbe0aa57623499582a2d19c1f00cdfa7c8ef0
11B-SHRINK <151459682+SPCDIAZRIVERACHRISTIAN@users.noreply.github.com>
2024-04-23 16:00:02 -0400

    Update simple_shell.c

--- simple_shell.c
+++ simple_shell.c
@@ -25,7 +25,7 @@ int main(void)
 		perror("Failed allocating memory");
 		exit(EXIT_FAILURE);
 	}
-    printf("Welcome to simple_shell use it if you dare :)\n");
+    //printf("Welcome to simple_shell use it if you dare :)\n");
 
     while (1)
     {
@@ -36,7 +36,7 @@ int main(void)
         {
             free(buffer);
 			free(args);
-            perror("C ya later! Don't forget your coffee!");
+            //perror("C ya later! Don't forget your coffee!");
             exit(EXIT_FAILURE);
         }
 
