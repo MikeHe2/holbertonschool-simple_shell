d40e9105f28c451f13cabab6974ed0960a54841e
Michael A. Hernandez <michael.he30@hotmail.com>
2024-04-23 14:02:25 -0400

    updated header main

--- main.h
+++ main.h
@@ -1,9 +1,17 @@
 #define MAIN_H
 #define MAIN_H
 
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <stdbool.h>
+
+#define MAX_ARGS 64
+
+char* get_command(char *cmd_name);
 
 #endif
