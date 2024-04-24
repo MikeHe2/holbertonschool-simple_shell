92ea147459b31291e71b848429678735a7eb6ecc
alexpr42 <8988@holbertonstudents.com>
2024-04-24 19:07:05 -0400

    fix in get_command file for betty

--- get_command.c
+++ get_command.c
@@ -9,34 +9,34 @@
 */
 char *get_command(char *cmd)
 {
-    static char *locations[] = {"/bin/", "/usr/bin/", NULL};
-    int i;
-    char *full_path;
+	static char *locations[] = {"/bin/", "/usr/bin/", NULL};
+	int i;
+	char *full_path;
 
-    if (access(cmd, X_OK) == 0)
-    {
-        full_path = malloc(strlen(cmd) + 1);
-        strcpy(full_path, cmd);
-        return full_path;
-    }
+	if (access(cmd, X_OK) == 0)
+	{
+		full_path = malloc(strlen(cmd) + 1);
+		strcpy(full_path, cmd);
+		return (full_path);
+	}
 
-    for (i = 0; locations[i] != NULL; i++)
-    {
-        full_path = malloc(strlen(locations[i]) + strlen(cmd) + 2);
-        if (full_path == NULL)
-        {
-            perror("Failed allocating memory");
-            exit(EXIT_FAILURE);
-        }
+	for (i = 0; locations[i] != NULL; i++)
+	{
+		full_path = malloc(strlen(locations[i]) + strlen(cmd) + 2);
+		if (full_path == NULL)
+		{
+			perror("Failed allocating memory");
+			exit(EXIT_FAILURE);
+		}
 
-        sprintf(full_path, "%s%s", locations[i], cmd);
+		sprintf(full_path, "%s%s", locations[i], cmd);
 
-        if (access(full_path, X_OK) == 0)
-        {
-            return full_path;
-        }
+		if (access(full_path, X_OK) == 0)
+		{
+			return full_path;
+		}
 
-        free(full_path);
-    }
-    return NULL;
+		free(full_path);
+	}
+	return NULL;
 }
