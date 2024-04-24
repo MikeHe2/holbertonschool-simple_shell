caddd5fc7e6aaad4d04ea43f09d4b02b5db06737
alexpr42 <8988@holbertonstudents.com>
2024-04-24 09:52:58 -0400

    update

--- simple_shell.c
+++ simple_shell.c
@@ -10,13 +10,13 @@ extern char **environ;
 
 int main(void)
 {
-    char *buffer = NULL;
-    char **args = NULL;
-    ssize_t read;
-    pid_t child;
-    size_t size = 0;
-    int status = 0;
-    char *token;
+	char *buffer = NULL;
+	char **args = NULL;
+	ssize_t read;
+	pid_t child;
+	size_t size = 0;
+	int status = 0;
+	char *token;
 	int i;
 
 	args = malloc(MAX_ARGS * sizeof(char*));
@@ -26,75 +26,75 @@ int main(void)
 		exit(EXIT_FAILURE);
 	}
 
-    while (1)
-    {
+	while (1)
+	{
 		if (isatty(0))
-        		printf("$ ");
+			printf("$ ");
 
-        i = 0;
-        read = getline(&buffer, &size, stdin);
-        if (read == -1)
-        {
-            free(buffer);
+		i = 0;
+		read = getline(&buffer, &size, stdin);
+		if (read == -1)
+		{
+			free(buffer);
 			free(args);
-            break;
-        }
+			break;
+		}
 
-        if (read == 1 && buffer[0] == '\n')
-            continue;
+		if (read == 1 && buffer[0] == '\n')
+			continue;
 
-        buffer[read - 1] = '\0';
+		buffer[read - 1] = '\0';
 
-        token = strtok(buffer, " \t");
-        while (token != NULL && i < MAX_ARGS - 1)
-        {
-            args[i++] = token;
-            token = strtok(NULL, " \t");
-        }
-        args[i] = NULL;
+		token = strtok(buffer, " \t");
+		while (token != NULL && i < MAX_ARGS - 1)
+		{
+			args[i++] = token;
+			token = strtok(NULL, " \t");
+		}
+		args[i] = NULL;
 
-        if (strcmp(args[0], "exit") == 0)
-        {
-            free(buffer);
+		if (strcmp(args[0], "exit") == 0)
+		{
+			free(buffer);
 			free(args);
-            break;
-        }
+			break;
+		}
 
-        child = fork();
-        if (child == -1)
-        {
-            perror("fork not successful");
+		child = fork();
+		if (child == -1)
+		{
+			perror("fork not successful");
 			free(args);
-            exit(EXIT_FAILURE);
-        }
-        else if (child == 0)
-        {
+			exit(EXIT_FAILURE);
+		}
+		else if (child == 0)
+		{
 				char *path = get_command(args[0]);
 
-            if (path != NULL)
-            {
+				if (path != NULL)
+				{
 				if (access(path, X_OK) == 0)
 				{
-                	execve(path, args, environ);
-                	perror("execve");
+					execve(path, args, environ);
+					perror("execve");
 					exit(EXIT_FAILURE);
 				}
-            }
-            else
-            {
-                printf("%s: command not found\n", buffer);
-            	free(path);
-            	exit(EXIT_FAILURE);
-            }
-        }
-        else
-        {
-            if (wait(&status) == -1)
-            {
-                perror("waitpid");
-                exit(EXIT_SUCCESS);
-            }
-        }
-    }
-    return (0);
+				}
+				else
+				{
+					printf("%s: command not found\n", buffer);
+					free(path);
+					exit(EXIT_FAILURE);
+				}
+		}
+		else
+		{
+			if (wait(&status) == -1)
+			{
+				perror("waitpid");
+				exit(EXIT_SUCCESS);
+			}
+		}
+	}
+	return (0);
 }
