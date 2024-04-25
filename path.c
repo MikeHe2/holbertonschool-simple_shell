75fc31561de306aa25d67298a464afe7dbfcafbd
alexpr42 <8988@holbertonstudents.com>
2024-04-25 00:41:53 -0400

    path.c file added

--- path.c
+++ path.c
@@ -1,27 +1,37 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
-int path_main(int argc, char *argv[]) 
+int pat_main(int argc, char *argv[])
 {
-	if (argc < 2) 
+	if (getenv("PATH") == NULL || strcmp(getenv("PATH"), "") == 0)
+	{
+		fprintf(stderr, "PATH is empty. Command execution is not possible.\n");
+		return EXIT_FAILURE;
+	}
+
+	if (argc < 2)
 	{
 		fprintf(stderr, "Usage: %s <arg1> [arg2] ...\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
 
-	if (strcmp(argv[1], "ls") == 0) 
+	if (strcmp(argv[1], "ls") == 0)
 	{
-		if (execvp("ls", &argv[1]) == -1) {
+		if (execvp("ls", &argv[1]) == -1)
+		{
 			perror("execvp");
 			exit(EXIT_FAILURE);
-		}
+        }
 	}
 	else
 	{
 		fprintf(stderr, "Command not supported: %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
+
 	return (0);
 }
+
