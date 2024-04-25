0372756dc0687151936424896f12964dd623146f
Christian <Christian>
2024-04-25 02:20:59 -0400

    reverted back to the way shell was

--- simple_shell.c
+++ simple_shell.c
@@ -9,42 +9,95 @@
 
 int main(void)
 {
-	char *buffer = NULL, **args = NULL;
+	char *buffer = NULL;
+	char **args = NULL;
 	ssize_t read;
 	pid_t child;
 	size_t size = 0;
 	int status = 0;
+	char *token;
+	int i;
 
-	args = fail_safe();
+	args = malloc(MAX_ARGS * sizeof(char *));
+	if (args == NULL)
+	{
+		perror("Failed allocating memory");
+		exit(EXIT_FAILURE);
+	}
 
 	while (1)
 	{
 		if (isatty(0))
 			printf("$ ");
 
+		i = 0;
 		read = getline(&buffer, &size, stdin);
 		if (read == -1)
 		{
+			free(buffer);
+			free(args);
 			break;
 		}
+
 		if (read == 1 && buffer[0] == '\n')
 			continue;
 
 		buffer[read - 1] = '\0';
 
-		tokenize_it(buffer, args);
+		token = strtok(buffer, " \t");
+		if (token == NULL)
+		{
+			continue;
+		}
+		while (token != NULL && i < MAX_ARGS - 1)
+		{
+			args[i++] = token;
+			token = strtok(NULL, " \t");
+		}
+		args[i] = NULL;
 
 		if (strcmp(args[0], "exit") == 0)
 		{
+			free(buffer);
+			free(args);
 			break;
 		}
-		child = give_birth(args);
-		free(args);
-	}
-		 if (waitpid(child, &status, 0) == -1)
+
+		child = fork();
+		if (child == -1)
 		{
-            perror("waitpid");
-            exit(EXIT_FAILURE);
-        }
-    return (0);
+			perror("fork not successful");
+			free(args);
+			exit(EXIT_FAILURE);
+		}
+		else if (child == 0)
+		{
+				char *path = get_command(args[0]);
+
+				if (path != NULL)
+				{
+				if (access(path, X_OK) == 0)
+				{
+					execve(path, args, environ);
+					perror("execve");
+					exit(EXIT_FAILURE);
+				}
+				}
+				else
+				{
+					printf("%s: command not found\n", buffer);
+					free(path);
+					exit(EXIT_FAILURE);
+				}
+		}
+		else
+		{
+			if (wait(&status) == -1)
+			{
+				perror("waitpid");
+				exit(EXIT_SUCCESS);
+			}
+		}
+	}
+	return (0);
 }
