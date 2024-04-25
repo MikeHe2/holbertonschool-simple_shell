18e3696a6529885bc3535b49d0b5d5d27b8e33ac
alexpr42 <8988@holbertonstudents.com>
2024-04-25 01:19:59 -0400

    fix to file path.c

--- path.c
+++ path.c
@@ -1,20 +1,34 @@
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 int pat_main(int argc, char *argv[])
 {
-	if (argc < 2) {
+	int i;
+	char *ls_paths[] = {"/bin/ls", "/usr/bin/ls", NULL};
+
+	if (argc < 2)
+	{
 		fprintf(stderr, "Usage: %s <arg1> [arg2] ...\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
 
-	if (execvp("ls", argv) == -1)
+	for (i = 0; ls_paths[i] != NULL; i++)
 	{
-		perror("execvp");
-		exit(EXIT_FAILURE);
+		if (access(ls_paths[i], X_OK) == 0)
+		{
+			argv[0] = ls_paths[i];
+			if (execvp(ls_paths[i], argv) == -1)
+			{
+				perror("execvp");
+				exit(EXIT_FAILURE);
+			}
+		}
 	}
 
-	return (0);
+	fprintf(stderr, "ls command not found\n");
+	exit(EXIT_FAILURE);
+
+	return 0;
 }
